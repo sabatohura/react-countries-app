20671a328897373240ee66a971e2fca420119c61
sabatohura <sabato@hura.one>
2023-08-13 00:56:27 +0200

    Create Toggle to change modes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,24 @@
 import React, {useState} from 'react'
+import {BsMoonStarsFill} from 'react-icons/bs'
 
 const Home = () => {
-    const [mode, setMode] = useState(false);
+    const [darkMode, setdarkMode] = useState(true);
 
-    
+
   return (
-    <div className=''>Home</div>
+    <>
+    <div className={`w-full h-screen ${darkMode ? 'bg-ThemeColor-DmVeryDarkBlue text-ThemeColor-BWhite' : 'bg-ThemeColor-LmVeryLightGray text-ThemeColor-LmVeryDarkBlue'} `}>
+        <div className={`main-screen flex justify-between ${darkMode ? 'bg-ThemeColor-DmDarkBlue' : 'bg-ThemeColor-BWhite'} `} >
+            <div>
+                <h2 className='font-bold text-xl '>Where in The world?</h2>
+            </div>
+                <BsMoonStarsFill onClick={()=>setdarkMode(!darkMode)} />
+        </div>
+        <div className='main-screen '>
+            body
+        </div>
+    </div>
+    </>
   )
 }
 
