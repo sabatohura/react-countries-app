909a0d5653e24de88769497bcc66b092bab80a4f
sabatohura <sabato@hura.one>
2023-08-13 00:16:54 +0200

    Create Home Component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React, {useState} from 'react'
+
+const Home = () => {
+    const [mode, setMode] = useState(false);
+
+    
+  return (
+    <div className=''>Home</div>
+  )
+}
+
+export default Home
